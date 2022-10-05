cdf8896063f8f50cbb7462339144847b847c463b
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-10-05 17:44:33 +0300

    Add hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,8 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+const config: HardhatUserConfig = {
+  solidity: "0.8.13",
+};
+
+export default config;
